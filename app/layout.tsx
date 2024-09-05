50b730fad0fdb43b343aa793aaee9df68f847bc7
Nils Cercariolo <nils.cercariolo@gmail.com>
2024-09-06 00:32:31 +0200

    modified and added icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,14 @@ export const metadata: Metadata = {
       url: "https://www.instagram.com/poneyclubdesportis/",
     },
   ],
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+  },
   robots: "index, follow",
 };
 
